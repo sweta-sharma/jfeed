206874ea79d7598c96fe901d4f8b68c01328c789
ishaaggarwal <isha.aggarwal@joshworkz.com>
2016-10-21 13:10:00 +0530

    Get youtube data Json

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    options = { developer_key: 'client = Yourub::Client.new(options)',
+    options = { developer_key: 'AIzaSyAKDK7oeN_5gUelYWtCyRCk6Im1dy3bGVs',
                 application_name: 'jfeed',
                 application_version: 2.0,
                 log_level: 3 }
     client = Yourub::Client.new(options)
-    client.search(country: "DE", category: "sports", order: 'date') do |v|
+    client.search(query: "funny cats") do |v|
       puts v
     end
   end
